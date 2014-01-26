b5efe8a332860c1f2ad6a139e19fbdf2c1d79804
dswebb <d.webb@nym.hush.com>
2014-01-26 22:17:49 +0000

    fixing syntax error

--- cookbooks/apache_vhosts/attributes/default.rb
+++ cookbooks/apache_vhosts/attributes/default.rb
@@ -5,8 +5,7 @@ default['apache_vhost']['wordpress'] => {
 						'ServerName'        => 'cms-deploy-test-lin',
 						'ServerAlias'        => ['cms-deploy-test-lin', 'lin-test.rackstage.co.uk'],
 						'DocumentRoot'        => '/var/www/vhosts/lin-test.rackstage.co.uk/public_html'
-					},
-				     ]
+					}
 #default['apache_vhost']['ServerName']        = 'cms-deploy-test-lin'
 #default['apache_vhost']['ServerAlias']        = ['cms-deploy-test-lin', 'lin-test.rackstage.co.uk']
 #default['apache_vhost']['DocumentRoot']        = '/var/www/vhosts/lin-test.rackstage.co.uk/public_html'
