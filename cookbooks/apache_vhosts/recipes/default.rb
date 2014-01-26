db3ba5421c4d707d92e2c3bf7500f242424ba77d
dswebb <d.webb@nym.hush.com>
2014-01-26 22:26:09 +0000

    s

--- cookbooks/apache_vhosts/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-# create VirtualHosts
-node['apache_vhosts'].each{|vhost| 
-	web_app vhost['app'] do
-		template 'vhost.erb'
-		docroot vhost['path']
-		server_name vhost['server_name']
-		server_email vhost['server_email']
-	end
-}
