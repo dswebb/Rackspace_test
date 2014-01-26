39e7bbd45aea65f40e9aa6cf0032c5ae180dfd33
dswebb <d.webb@nym.hush.com>
2014-01-26 22:19:45 +0000

    fixing syntax error

--- cookbooks/apache_vhosts/attributes/default.rb
+++ cookbooks/apache_vhosts/attributes/default.rb
@@ -1,11 +1,8 @@
 default['apache_vhost']['config_prefix'] = '/etc/apache2/sites-enabled'
-default['apache_vhost']['DocumentRoot']        = '/var/www/vhosts/lin-test.rackstage.co.uk/public_html'
-default['apache_vhost']['wordpress'] => {
-						'ServerAdmin'        => '',
-						'ServerName'        => 'cms-deploy-test-lin',
-						'ServerAlias'        => ['cms-deploy-test-lin', 'lin-test.rackstage.co.uk'],
-						'DocumentRoot'        => '/var/www/vhosts/lin-test.rackstage.co.uk/public_html'
-					}
+default['apache_vhost']['wordpress']['ServerAdmin'] = '',
+default['apache_vhost']['wordpress']['ServerName'] = 'cms-deploy-test-lin'
+default['apache_vhost']['wordpress']['ServerAlias'] = ['cms-deploy-test-lin', 'lin-test.rackstage.co.uk']
+default['apache_vhost']['wordpress']['DocumentRoot'] = '/var/www/vhosts/lin-test.rackstage.co.uk/public_html'
 #default['apache_vhost']['ServerName']        = 'cms-deploy-test-lin'
 #default['apache_vhost']['ServerAlias']        = ['cms-deploy-test-lin', 'lin-test.rackstage.co.uk']
 #default['apache_vhost']['DocumentRoot']        = '/var/www/vhosts/lin-test.rackstage.co.uk/public_html'
