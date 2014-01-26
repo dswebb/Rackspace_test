7e6403366d58a24fc2105760bf175aa6e6c3d042
dswebb <d.webb@nym.hush.com>
2014-01-26 21:31:31 +0000

    switch to variables in apache_vhost recipe

---
+++ cookbooks/apache_vhosts/recipes/wordpress.rb
@@ -0,0 +1,21 @@
+# create VirtualHosts
+
+prefix = node['authorization']['sudo']['prefix']
+
+node['apache_vhosts'].each{|vhost| 
+template "#{prefix}/".vhost.".conf" do
+  source 'vhost.erb'
+  mode   '0440'
+  owner  'root'
+  group  node['root_group']
+  variables(
+    :docroot    => node['apache_vhost'][vhost]['DocumentRoot'],
+    :serveradmin     => node['apache_vhost'][vhost]['ServerAdmin'],
+    :servername     => node['apache_vhost'][vhost]['ServerName'],
+    :serveralias      => node['apache_vhost'][vhost]['ServerAlias'],
+  )
+end
+
+}
+
+
