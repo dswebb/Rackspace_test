6953771c982cfa8e768c854b88a0eb05e7ed9337
dswebb <d.webb@nym.hush.com>
2014-01-26 21:38:23 +0000

    fixing syntax error

--- cookbooks/apache_vhosts/recipes/wordpress.rb
+++ cookbooks/apache_vhosts/recipes/wordpress.rb
@@ -1,21 +1,17 @@
 # create VirtualHosts
 
-prefix = node['authorization']['sudo']['prefix']
+prefix = node['apache_vhosts']['prefix']
 
-node['apache_vhosts'].each{|vhost| 
-template "#{prefix}/#{vhost}.conf" do
-  source 'vhost.erb'
-  mode   '0440'
-  owner  'root'
-  group  node['root_group']
-  variables(
-    :docroot    => node['apache_vhost'][vhost]['DocumentRoot'],
-    :serveradmin     => node['apache_vhost'][vhost]['ServerAdmin'],
-    :servername     => node['apache_vhost'][vhost]['ServerName'],
-    :serveralias      => node['apache_vhost'][vhost]['ServerAlias'],
-  )
+node['apache_vhosts']['vhosts'].each do |vhost| 
+  template "#{prefix}/#{vhost}.conf" do
+    source 'vhost.erb'
+    mode   '0440'
+    owner  'root'
+    group  node['root_group']
+    variables(
+      :docroot    => node['apache_vhost'][vhost]['DocumentRoot'],
+      :serveradmin     => node['apache_vhost'][vhost]['ServerAdmin'],
+      :servername     => node['apache_vhost'][vhost]['ServerName'],
+      :serveralias      => node['apache_vhost'][vhost]['ServerAlias'],
+    )
 end
-
-}
-
-
