dd3d3b059e4c18b0a41a5d9d1b6258f4f9625229
dswebb <d.webb@nym.hush.com>
2014-01-26 21:41:37 +0000

    blah

--- cookbooks/apache_vhosts/attributes/default.rb
+++ cookbooks/apache_vhosts/attributes/default.rb
@@ -6,7 +6,7 @@ default['apache_vhost']['vhosts'] = {
 						'ServerName'        => 'cms-deploy-test-lin',
 						'ServerAlias'        => ['cms-deploy-test-lin', 'lin-test.rackstage.co.uk'],
 						'DocumentRoot'        => '/var/www/vhosts/lin-test.rackstage.co.uk/public_html'
-					}
+					},
 				     }
 #default['apache_vhost']['ServerName']        = 'cms-deploy-test-lin'
 #default['apache_vhost']['ServerAlias']        = ['cms-deploy-test-lin', 'lin-test.rackstage.co.uk']
