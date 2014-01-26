56e1c79c2edb8e100878af2a6ff114646d7ba8f0
dswebb <d.webb@nym.hush.com>
2014-01-26 21:46:09 +0000

    changing from hash of hashes to array of hashes

--- cookbooks/apache_vhosts/attributes/default.rb
+++ cookbooks/apache_vhosts/attributes/default.rb
@@ -1,13 +1,13 @@
 default['apache_vhost']['config_prefix'] = '/etc/apache2/sites-enabled'
 default['apache_vhost']['DocumentRoot']        = '/var/www/vhosts/lin-test.rackstage.co.uk/public_html'
-default['apache_vhost']['vhosts'] = { 
+default['apache_vhost']['vhosts'] = [ 
 					'wordpress' => {
 						'ServerAdmin'        => '',
 						'ServerName'        => 'cms-deploy-test-lin',
 						'ServerAlias'        => ['cms-deploy-test-lin', 'lin-test.rackstage.co.uk'],
 						'DocumentRoot'        => '/var/www/vhosts/lin-test.rackstage.co.uk/public_html'
 					},
-				     }
+				     ]
 #default['apache_vhost']['ServerName']        = 'cms-deploy-test-lin'
 #default['apache_vhost']['ServerAlias']        = ['cms-deploy-test-lin', 'lin-test.rackstage.co.uk']
 #default['apache_vhost']['DocumentRoot']        = '/var/www/vhosts/lin-test.rackstage.co.uk/public_html'
