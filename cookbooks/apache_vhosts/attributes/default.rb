cc9dd5d7a2493bb4d17279d469c9c24f2bc2b987
dswebb <d.webb@nym.hush.com>
2014-01-26 21:13:12 +0000

    updating vhost attributes

---
+++ cookbooks/apache_vhosts/attributes/default.rb
@@ -0,0 +1,11 @@
+default['apache_vhost']['vhosts'] = { 
+					'wordpress' = {
+						'ServerAdmin'        = '',
+						'ServerName'        = 'cms-deploy-test-lin',
+						'ServerAlias'        = ['cms-deploy-test-lin', 'lin-test.rackstage.co.uk'],
+						'DocumentRoot'        = '/var/www/vhosts/lin-test.rackstage.co.uk/public_html'
+					}
+				     }
+#default['apache_vhost']['ServerName']        = 'cms-deploy-test-lin'
+#default['apache_vhost']['ServerAlias']        = ['cms-deploy-test-lin', 'lin-test.rackstage.co.uk']
+#default['apache_vhost']['DocumentRoot']        = '/var/www/vhosts/lin-test.rackstage.co.uk/public_html'
