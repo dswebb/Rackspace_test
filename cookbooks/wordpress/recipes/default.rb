51c4acd048562a56de799d65d2f5224b2e6317cd
dswebb <d.webb@nym.hush.com>
2014-01-26 22:59:23 +0000

    updating wordpress default.rb

--- cookbooks/wordpress/recipes/default.rb
+++ cookbooks/wordpress/recipes/default.rb
@@ -79,7 +79,7 @@ else
   end
 
   execute "extract-wordpress" do
-    command "tar xf #{Chef::Config[:file_cache_path]}/#{archive} -C #{node['wordpress']['parent_dir']}"
+    command "tar xf #{Chef::Config[:file_cache_path]}/#{archive} --strip-components 1 -C #{node['wordpress']['parent_dir']}"
     creates "#{node['wordpress']['dir']}/index.php"
   end
 end
